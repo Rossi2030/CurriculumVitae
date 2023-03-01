5ca834a2a4fdc14bdd20b5c60a84fdf0e7a89ea9
Joaquin <joaquin_20_30@hotmail.com>
2023-03-01 20:29:01 -0300

    primer commit

---
+++ main.js
@@ -0,0 +1,25 @@
+function mensaje() {
+    swal("Me recibí en el año 2017 con un alto promedio")
+    
+}
+
+function primera() {
+    swal("Estuve como Ayudante General. " + "En la Resolución de problemas. " 
+    + "En la Atención al Cliente.")
+    
+}
+
+function segunda() {
+    swal("Trabaje en la supervisión de las cuentas para comprobar pagos. " 
+    + "En los cobros y pagos a clientes, agencias, proveedores y acreedores.")
+    
+}
+
+let tercero = document.getElementById("tercero")
+tercero.innerText = "Tercera Experiencia"
+
+function tercera() {
+    swal("Me desenpeñe como supervisor de cálculos en impuestos anuales, provisionales y definitivos " 
+    + "de los diferentes impuestos, contabilidad y atención a requerimientos.")
+    
+}
